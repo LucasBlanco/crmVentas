983eaaa15cdf4343b0e931df53ea1db73255e6db
LucasBlanco <lucas_manuel_blanco@hotmail.com>
2019-12-20 17:44:24 -0300

    la tabla de sesion operador se puede ordenar por nombre

--- src/app/views/pages/control-operadoras/control-operadoras.module.ts
+++ src/app/views/pages/control-operadoras/control-operadoras.module.ts
@@ -1,24 +1,26 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule, MatSortModule, MatTableModule } from '@angular/material';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { RouterModule, Routes } from '@angular/router';
+
 import { TablaControlComponent } from './tabla-control/tabla-control.component';
-import {RouterModule, Routes} from "@angular/router";
-import {MatCardModule, MatTableModule} from "@angular/material";
-import {MatChipsModule} from "@angular/material/chips";
-import {MatPaginatorModule} from "@angular/material/paginator";
 
 
 const routes: Routes = [
 	{ path: '', component: TablaControlComponent }
 ];
 @NgModule({
-  declarations: [TablaControlComponent],
-  imports: [
-    CommonModule,
-	  RouterModule.forChild(routes),
-	  MatCardModule,
-	  MatTableModule,
-	  MatPaginatorModule,
-	  MatChipsModule
-  ]
+	declarations: [TablaControlComponent],
+	imports: [
+		CommonModule,
+		RouterModule.forChild(routes),
+		MatCardModule,
+		MatTableModule,
+		MatPaginatorModule,
+		MatChipsModule,
+		MatSortModule
+	]
 })
 export class ControlOperadorasModule { }
--- src/app/views/pages/control-operadoras/tabla-control/tabla-control.component.ts
+++ src/app/views/pages/control-operadoras/tabla-control/tabla-control.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { SesionOperador, SesionOperadorService } from '@servicios/sesion-operador.service';
 import * as moment from 'moment';
@@ -13,28 +14,29 @@ import * as moment from 'moment';
 })
 export class TablaControlComponent implements OnInit {
 
+	@ViewChild(MatSort, { static: true }) sort: MatSort;
 	dataSource = new MatTableDataSource<SesionOperador>([]);
 	constructor(private sesionOperadorSrv: SesionOperadorService, public dialog: MatDialog) { }
 
 	ngOnInit() {
 		this.sesionOperadorSrv.traerTodos().subscribe(operadores => {
 			this.dataSource = new MatTableDataSource<SesionOperador>(operadores);
-		})
-
+			this.dataSource.sort = this.sort;
+		});
 	}
 
 
 
 	formatHoraActividad(fecha) {
 		if (fecha) {
-			let minutos = Math.abs(moment(fecha, "YYYY-MM-DD HH:mm:ss").diff(moment(), "minutes"))
+			const minutos = Math.abs(moment(fecha, 'YYYY-MM-DD HH:mm:ss').diff(moment(), 'minutes'));
 			if (minutos < 60) {
-				return "Hace " + minutos + " minutos";
+				return 'Hace ' + minutos + ' minutos';
 			} else {
 				if (minutos % 60 > 0) {
-					return "Hace " + Math.trunc(minutos / 60) + "hora/s y " + minutos % 60 + "minutos"
+					return 'Hace ' + Math.trunc(minutos / 60) + 'hora/s y ' + minutos % 60 + 'minutos';
 				} else {
-					return "Hace " + Math.trunc(minutos / 60) + "hora/s"
+					return 'Hace ' + Math.trunc(minutos / 60) + 'hora/s';
 				}
 			}
 		}
@@ -43,12 +45,12 @@ export class TablaControlComponent implements OnInit {
 
 	determinarChipEstado(estado) {
 		switch (estado) {
-			case "Conectado":
-				return "estadoConectado"
-			case "Desconectado":
-				return "estadoDesconectado"
-			case "Break":
-				return "estadoBreak"
+			case 'Conectado':
+				return 'estadoConectado';
+			case 'Desconectado':
+				return 'estadoDesconectado';
+			case 'Break':
+				return 'estadoBreak';
 		}
 	}
 
